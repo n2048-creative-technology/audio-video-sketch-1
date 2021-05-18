e6cb462d5d769e68dac30cc26c38bbd5a499283f
Mauricio van der Maesen de Sombreff <mauricio@mauricios-mbp-2.lan>
2021-05-18 10:10:03 +0200

    merge stepper and encoder scripts

--- encoder-to-serial/encoder-to-serial.ino
+++ encoder-to-serial/encoder-to-serial.ino
@@ -11,12 +11,45 @@ RotaryEncoder encoder(PIN_IN1, PIN_IN2, RotaryEncoder::LatchMode::TWO03);
 
 int lastPos = -1;
 
+
+
+#define stepperPulsePin 9 // green
+#define stepperDirectionPin 10 // white
+#define stepperEnablePin 11 // blue
+
+volatile boolean stepper_direction = LOW;
+volatile boolean steper_pulse_state = LOW;
+
+volatile boolean run_mode = LOW;
+
+volatile int counter = 0;
+
+
 void setup()
 {
   Serial.begin(115200);
   while (! Serial);
 
   encoder.setPosition(0);
+
+  cli();
+  TCCR0A = 0;// set entire TCCR0A register to 0
+  TCCR0B = 0;// same for TCCR0B
+  TCNT0  = 0;//initialize counter value to 0
+  OCR0A = 124;// = (16*10^6) / (2000*64) - 1 (must be <256)
+  TCCR0A |= (1 << WGM01);
+  TCCR0B |= (1 << CS01) | (1 << CS00);
+  TIMSK0 |= (1 << OCIE0A);
+
+  pinMode(stepperPulsePin, OUTPUT);
+  pinMode(stepperDirectionPin, OUTPUT);
+  pinMode(stepperEnablePin, OUTPUT);
+
+  sei();
+
+  digitalWrite(stepperEnablePin, run_mode);
+  digitalWrite(stepperDirectionPin, LOW);
+  digitalWrite(stepperPulsePin, LOW);
 }
 
 
@@ -37,5 +70,18 @@ void loop()
 
     Serial.print(lastPos );
     Serial.println();
+  }
+}
+
+
+ISR(TIMER0_COMPA_vect) { //timer0 interrupt 2kHz toggles pin 8
+  steper_pulse_state = !steper_pulse_state;
+  digitalWrite(stepperDirectionPin, HIGH);
+  digitalWrite(stepperPulsePin, steper_pulse_state);
+  digitalWrite(stepperEnablePin, run_mode);
+  counter ++;
+  if (counter > 2000) {
+    run_mode = !run_mode;
+    counter = 0;
   }
 }
